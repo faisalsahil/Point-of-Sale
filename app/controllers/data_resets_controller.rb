04183fc7dbcadde49fa09626655f95dcfe734e77
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2019-04-09 17:43:06 +0500

    reset db issue resolved

--- app/controllers/data_resets_controller.rb
+++ app/controllers/data_resets_controller.rb
@@ -4,8 +4,9 @@ class DataResetsController < ApplicationController
   end
 
   def create
-    d = params[:to_date].to_date
-    orders = Order.where('created_at <= ?', d)
+
+    d      = params[:to_date].to_date
+    orders = Order.where('extract(year from created_at) = ? AND  extract(month from created_at) <= ?', d.year, d.month)
     orders&.each do |order|
       order.order_products.destroy_all
       order.destroy
